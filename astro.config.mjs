972663bb028d8935761756f82e8990b510019e76
Troy <hello@troylusty.com>
2024-10-08 16:56:49 +0100

    Add prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://uop-gameplay.github.io",
-  base: "website"
+  base: "website",
   integrations: [mdx(), sitemap()],
 });
