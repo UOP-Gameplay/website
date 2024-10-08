64f27e7f808f7a3b1c10a27810d093c635feecb9
Troy <hello@troylusty.com>
2024-10-08 20:03:46 +0100

    Add astro icon plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://uop-gameplay.github.io",
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
 });
