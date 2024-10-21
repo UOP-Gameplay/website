9f1a9c4fe6a2cb54d1406d5f3675036f48d47e3f
Troy <hello@troylusty.com>
2024-10-21 11:25:30 +0100

    Use passthroughImageService

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://uop-gameplay.github.io",
+  site: "https://retailtherapy.troylusty.com",
   integrations: [tailwind(), icon()],
   output: "hybrid",
   adapter: node({
     mode: "standalone",
   }),
+  image: {
+    service: passthroughImageService(),
+  },
 });
