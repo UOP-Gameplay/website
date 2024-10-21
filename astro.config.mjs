e7d1c3e1147954e948b43f557973c9e3d862e4cb
Troy <hello@troylusty.com>
2024-10-21 09:40:03 +0100

    Switch to using node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://uop-gameplay.github.io",
   integrations: [tailwind(), icon()],
+  output: "hybrid",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
