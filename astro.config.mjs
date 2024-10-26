f50524119888f7d6a3d73ba7d2e2685b5edbfff6
Troy <hello@troylusty.com>
2024-10-26 23:31:32 +0100

    Add astro-embed plugin for YouTube videos

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import node from "@astrojs/node";
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://retailtherapy.troylusty.com",
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), mdx()],
   output: "hybrid",
   adapter: node({
     mode: "standalone",
