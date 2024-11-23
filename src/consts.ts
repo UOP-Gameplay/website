0b3f271b6cf00b14b50fc4133a065d36e2e32b84
Troy <hello@troylusty.com>
2024-11-23 23:02:25 +0000

    Change site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Retail Therapy";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION =
+  "As the new employee at a clothing outlet store, your mission is to fulfil customer orders by carefully collecting the right items — or snagging the wrong one in a moment of frustration.";
